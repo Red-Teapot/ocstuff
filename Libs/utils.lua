457ac7ac42e3461f608f740c204f401b1555b73c
Nikita Medvedev <some.redteapot@gmail.com>
2020-03-26 21:13:45 +0700

    Create utils library

---
+++ Libs/utils.lua
@@ -0,0 +1,33 @@
+local serialization = require('serialization')
+
+local utils = {}
+
+function utils.sign(x)
+    if x > 0 then
+        return 1
+    elseif x == 0 then
+        return 0
+    else
+        return 1
+    end
+end
+
+function utils.merge(dst, src)
+    for key, value in pairs(src) do
+        local t = type(value)
+
+        if t == 'table' then
+            utils.merge(dst[key], value)
+        elseif t == 'nil' then
+            -- Do nothing
+        else
+            dst[key] = value
+        end
+    end
+end
+
+function utils.pprint(var)
+    print(serialization.serialize(var, true))
+end
+
+return utils
