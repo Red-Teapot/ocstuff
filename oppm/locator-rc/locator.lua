01cec3bd4e0644061b4f93ab53d9bc8a7f475d09
Nikita Medvedev <some.redteapot@gmail.com>
2020-04-03 17:40:16 +0700

    Fix locator so it handles cfg=nil properly

--- oppm/locator-rc/locator.lua
+++ oppm/locator-rc/locator.lua
@@ -3,6 +3,7 @@ local positiond = require('positiond')
 local vec3 = require('vec3')
 
 function start(cfg)
+    if not cfg then cfg = {} end
     local x, y, z = gps.locate(cfg.responsePort, cfg.timeout, cfg.maxTries, cfg.satellitePort)
 
     if x ~= nil and y ~= nil and z ~= nil then
