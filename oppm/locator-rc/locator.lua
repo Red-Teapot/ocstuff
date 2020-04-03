74477c498abba6b65e2e03df1e78f0ad5aac628f
Nikita Medvedev <some.redteapot@gmail.com>
2020-04-03 17:34:53 +0700

    Implement locator service

---
+++ oppm/locator-rc/locator.lua
@@ -0,0 +1,11 @@
+local gps = require('gps')
+local positiond = require('positiond')
+local vec3 = require('vec3')
+
+function start(cfg)
+    local x, y, z = gps.locate(cfg.responsePort, cfg.timeout, cfg.maxTries, cfg.satellitePort)
+
+    if x ~= nil and y ~= nil and z ~= nil then
+        positiond.setPosition(vec3.new(x, y, z))
+    end
+end
